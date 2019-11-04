89e1c1c9baa261b49b0140a5519b91c65c5ff2ee
cl33per <leeper08@gmail.com>
2019-11-03 20:31:13 -0500

    logic adjustments

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friends = [{
         "name": "David",
     "photo": "https://www.visittuscany.com/shared/visittuscany/immagini/blogs/idea/david-michelangelo-accademia.jpg?__scale=h:1488,w:1998,cx:4,cy:12,ch:1488,cw:1998",
-        "scores": [3, 1, 2, 4, 2, 1, 2, 2, 1, 1]
+        "scores": [3, 1, 2, 4, 2, 1, 2, 2, 1]
     },
     {
         "name": "Donald Duck",
         "photo": "https://img.etimg.com/thumb/msid-58980445,width-643,imgsize-182458,resizemode-4/some-fun-facts-about-disneys-most-popular-character-donald-duck.jpg",
-        "scores": [1, 1, 1, 4, 2, 1, 2, 1, 2, 1]
+        "scores": [1, 1, 1, 4, 2, 1, 2, 1, 2]
     },
     {
         "name": "Luis Rossman",
         "photo": "http://www.mackungfu.org/dump/louislouis.jpg",
-        "scores": [5, 5, 4, 5, 5, 1, 3, 5, 4, 5]
+        "scores": [5, 5, 4, 5, 5, 1, 3, 5, 4]
     }
 ]
 
